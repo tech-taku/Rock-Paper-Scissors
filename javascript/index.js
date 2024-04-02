5f6a899ce3e08225f1dd12e2bb18009f45debd23
tech-taku <taku.map25@gmail.com>
2024-04-03 01:07:30 +0200

    Add Score tracking to the game

--- javascript/index.js
+++ javascript/index.js
@@ -1,24 +1,37 @@
 const choices = ['Rock', 'Paper', 'Scissors']
 
+let playerScore = 0;
+let computerScore = 0;
+
 // Declare an array choices to select our choices from.
+for (let i = 0; i < 5; i++) {
+    const playerChoice = prompt("This is a 'Rock', 'Paper', 'Scissors' game. Which do you choose? ").toLowerCase();
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)].toLowerCase();
 
-const playerChoice = prompt("This is a 'Rock', 'Paper', 'Scissors' game. Which do you choose? ").toLowerCase();
-
-const computerChoice = choices[Math.floor(Math.random() * choices.length)].toLowerCase();
-
-
-console.log('Player chose: ', playerChoice)
-console.log('Computer chose', computerChoice)
+    console.log('Player chose: ', playerChoice)
+    console.log('Computer chose', computerChoice)
 
 
 
-if (playerChoice === computerChoice) {
+    if (playerChoice === computerChoice) {
 
-    console.log(`Player chose ${playerChoice} and Computer chose ${computerChoice}. it is a draw`)
-} else if ((playerChoice === 'rock' && computerChoice === 'scissors') ||
-    (playerChoice === 'scissors' && computerChoice === 'paper') ||
-    (playerChoice === 'paper' && computerChoice === 'rock')) {
-    console.log(`Player choice is ${playerChoice} and Computer choice is ${computerChoice}. Player Wins`);
-} else {
-    console.log(`Computer choice is ${computerChoice} and Player choice is ${playerChoice}. Computer Wins`);
+        console.log(`Player chose ${playerChoice} and Computer chose ${computerChoice}. it is a draw`)
+    } else if ((playerChoice === 'rock' && computerChoice === 'scissors') ||
+        (playerChoice === 'scissors' && computerChoice === 'paper') ||
+        (playerChoice === 'paper' && computerChoice === 'rock')) {
+        console.log(`Player choice is ${playerChoice} and Computer choice is ${computerChoice}. Player Wins`);
+        playerScore++;
+    } else {
+        console.log(`Computer choice is ${computerChoice} and Player choice is ${playerChoice}. Computer Wins`);
+        computerScore++
+    }
+
+    console.log('Current Player Score: ', playerScore);
+    console.log('Current Computer Score: ', computerScore)
+
 }
+
+
+
+console.log('Final Score: ', playerScore);
+console.log('Final Score: ', computerScore);
