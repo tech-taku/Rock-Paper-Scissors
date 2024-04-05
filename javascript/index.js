6af9c0c899b56846fa63570506723d6f9ca1ab83
tech-taku <taku.map25@gmail.com>
2024-04-05 23:02:41 +0200

    Add DOM refrencing for each button node

--- javascript/index.js
+++ javascript/index.js
@@ -1,67 +1,49 @@
-// The User choice:
-
-const playerChoice = document.querySelectorAll("choices")
-
-console.log("choices are: ", playerChoice)
+const rockButton = document.querySelector("#rock-btn");
+const paperButton = document.querySelector("#paper-btn");
+const scissorsButton = document.querySelector("#scissors-btn");
 
 
+// when we click button say rock playerChoice becomes rock then computer choice becomes abc randomly picked...
 
+const generateComputerChoice = () => {
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)]
+}
 
+rockButton.addEventListener("click", () => {
+    const playerChoice = "rock";
+    const computerChoice = generateComputerChoice();
+    playRound(playerChoice, computerChoice)
+})
 
+paperButton.addEventListener("click", () => {
+    const playerChoice = "paper";
+    const computerChoice = generateComputerChoice();
+    playRound(playerChoice, computerChoice)
+})
 
+scissorsButton.addEventListener("click", () => {
+    const playerChoice = "scissors";
+    const computerChoice = generateComputerChoice()
+    playRound(playerChoice, computerChoice)
+})
 
+const playRound = (playerChoice, computerChoice) => {
+    // CHeck what has been picked and display a message
+    // let playerScore = 0;
+    // let computerScore = 0;
 
+    // while(playerScore <= 5 || computerScore <= 5){
+    if (playerChoice === computerChoice) {
 
+        const playersChoices = document.querySelector(".choices-message")
+        console.log(playersChoices)
+        // Manipuate the dom and show on browser
+        // const computerChoiceText = document.querySelector("#computer-selection");
+        // const playerChoiceText = document.querySelector("#player-selection");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const choices = ['Rock', 'Paper', 'Scissors']
-
-// let playerScore = 0;
-// let computerScore = 0;
-
-// // Declare an array choices to select our choices from.
-// for (let i = 0; i < 5; i++) {
-//     const playerChoice = prompt("This is a 'Rock', 'Paper', 'Scissors' game. Which do you choose? ").toLowerCase();
-//     const computerChoice = choices[Math.floor(Math.random() * choices.length)].toLowerCase();
-
-//     console.log('Player chose: ', playerChoice)
-//     console.log('Computer chose', computerChoice)
-
-
-
-//     if (playerChoice === computerChoice) {
-
-//         console.log(`Player chose ${playerChoice} and Computer chose ${computerChoice}. it is a draw`)
-//     } else if ((playerChoice === 'rock' && computerChoice === 'scissors') ||
-//         (playerChoice === 'scissors' && computerChoice === 'paper') ||
-//         (playerChoice === 'paper' && computerChoice === 'rock')) {
-//         console.log(`Player choice is ${playerChoice} and Computer choice is ${computerChoice}. Player Wins`);
-//         playerScore++;
-//     } else {
-//         console.log(`Computer choice is ${computerChoice} and Player choice is ${playerChoice}. Computer Wins`);
-//         computerScore++
-//     }
-
-//     console.log('Current Player Score: ', playerScore);
-//     console.log('Current Computer Score: ', computerScore)
-
-// }
-
-
-
-// console.log('Final Score: ', playerScore);
-// console.log('Final Score: ', computerScore);
+        // computerChoiceText.textContent = `Computer selected: ${computerChoice}`;
+        // playerChoiceText.textContent = `Player Selected: ${playerChoice}`;
+        // }
+    }
+}
