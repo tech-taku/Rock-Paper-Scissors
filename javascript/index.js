691826d26abf70b074697d6ea7843e60fe658310
tech-taku <taku.map25@gmail.com>
2024-04-07 11:36:01 +0200

    Add choice images to correctly displayer each player's choice side by side

--- javascript/index.js
+++ javascript/index.js
@@ -10,6 +10,65 @@ const generateComputerChoice = () => {
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
+const displayImage = (playerChoice, computerChoice) => {
+    const playerChoiceImg = document.querySelector(".player-choice-img");
+    const computerChoiceImg = document.querySelector(".computer-choice-img");
+    // const choiceButton = document.createElement("button");
+
+    playerChoiceImg.innerHTML = "";
+    computerChoiceImg.innerHTML = "";
+
+    const createImage = (src) => {
+        const choiceImg = document.createElement("img");
+        choiceImg.src = src;
+        choiceImg.style.width = "140px";
+        choiceImg.style.height = "auto";
+        return choiceImg;
+    }
+
+    const playerImg = createImage(`images/${playerChoice}.png`);
+    playerChoiceImg.appendChild(playerImg);
+
+    const computerImg = createImage(`images/${computerChoice}.png`);
+    computerChoiceImg.appendChild(computerImg);
+}
+
+const playRound = (playerChoice, computerChoice) => {
+
+    const resultMessage = document.querySelector("#results-message");
+    const roundMessage = roundMessages(playerChoice, computerChoice);
+
+    if (playerChoice === computerChoice) {
+        roundMessage;
+        resultMessage.textContent = "";
+        resultMessage.textContent = `It is a tie`;
+        displayImage(playerChoice, computerChoice);
+
+    } else if (playerChoice === "rock" && computerChoice === "scissors" ||
+        playerChoice === "scissors" && computerChoice === "paper" ||
+        playerChoice === "paper" && computerChoice === "rock") {
+        roundMessage;
+        resultMessage.textContent = "";
+        resultMessage.textContent = `You win!`;
+        displayImage(playerChoice, computerChoice);
+
+        console.log(`player choice: ${playerChoice}, computer choice: ${computerChoice}`);
+    } else {
+        console.log(`player choice: ${playerChoice}, computer choice: ${computerChoice}`);
+        roundMessage;
+        resultMessage.textContent = "";
+        resultMessage.textContent = `Computer Wins!`;
+        displayImage(playerChoice, computerChoice)
+
+    }
+}
+
+const roundMessages = (playerChoice, computerChoice) => {
+    const playersChoices = document.querySelector("#choices-message");
+    playersChoices.textContent = "";
+    playersChoices.textContent = `You chose ${playerChoice} and computer chose ${computerChoice}`;
+}
+
 rockButton.addEventListener("click", () => {
     const playerChoice = "rock";
     const computerChoice = generateComputerChoice();
@@ -27,36 +86,3 @@ scissorsButton.addEventListener("click", () => {
     const computerChoice = generateComputerChoice()
     playRound(playerChoice, computerChoice)
 })
-
-const playRound = (playerChoice, computerChoice) => {
-
-    const resultMessage = document.querySelector("#results-message");
-    const roundMessage = roundMessages(playerChoice, computerChoice);
-
-    if (playerChoice === computerChoice) {
-        roundMessage;
-        resultMessage.textContent = "";
-        resultMessage.textContent = `It is a tie`;
-
-    } else if (playerChoice === "rock" && computerChoice === "scissors" ||
-        playerChoice === "scissors" && computerChoice === "paper" ||
-        playerChoice === "paper" && computerChoice === "rock") {
-        roundMessage;
-        resultMessage.textContent = "";
-        resultMessage.textContent = `You win!`;
-
-        console.log(`player choice: ${playerChoice}, computer choice: ${computerChoice}`);
-    } else {
-        console.log(`player choice: ${playerChoice}, computer choice: ${computerChoice}`);
-        roundMessage;
-        resultMessage.textContent = "";
-        resultMessage.textContent = `Computer Wins!`;
-
-    }
-}
-
-const roundMessages = (playerChoice, computerChoice) => {
-    const playersChoices = document.querySelector("#choices-message");
-    playersChoices.textContent = "";
-    playersChoices.textContent = `You chose ${playerChoice} and computer chose ${computerChoice}`;
-}
